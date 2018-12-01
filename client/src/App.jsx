440ccf992ae98643020bf9dec2a2aaffdbee6ebb
step-off <nofantasy12@gmail.com>
2018-12-01 13:08:36 +0500

    simple component

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const pair = ['Trainspotting', '28 Days Later'];
+
+export class App extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return <h1>Hello from App</h1>
+    }
+}
